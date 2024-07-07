84cac045acdce1a4947373d76960b1d2bb027423
Aliaksandr Fedukovich <1632637@mail.ru>
2024-07-07 18:20:48 +0300

    feat: add fetch

--- rs-school-react/src/App.tsx
+++ rs-school-react/src/App.tsx
@@ -1,14 +1,33 @@
+import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
+import { MovieDescription } from './types';
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Main />
-    </div>
-  );
+export type SetMovieArrFunction = (arr: MovieDescription[]) => void;
+
+export class App extends React.Component {
+  moviesArray: MovieDescription[] = [];
+
+  constructor(props: {}) {
+    super(props);
+    this.setMoviesArray = this.setMoviesArray.bind(this);
+  }
+
+  setMoviesArray(arr: MovieDescription[]) {
+    this.moviesArray = arr;
+    // Возможно, вам нужно будет что-то делать с moviesArray, например обновлять состояние
+    console.log(this.moviesArray);
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className="app">
+        <Header setMovieArray={this.setMoviesArray} />
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
--- rs-school-react/src/components/header/Header.tsx
+++ rs-school-react/src/components/header/Header.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import './styles.css';
+import { MovieDescription } from '../../types';
 
 /* https://www.omdbapi.com/?apikey=7c372dd6&&type=movie&s=terminator+2 */
 /* https://omdbapi.com/ */
 
-export default class Header extends React.Component {
-  searchRequest = '';
+export type setMovieArrayType = (arr: MovieDescription[]) => void;
 
-  constructor() {
-    super(1);
+export interface HeaderProps {
+  setMovieArray: setMovieArrayType;
+}
+
+type Props = Readonly<HeaderProps>;
+
+export default class Header extends React.Component<Props> {
+  searchRequest: string;
+
+  constructor(props: Props) {
+    super(props);
     this.searchRequest = localStorage.getItem('requestApi') || '';
+
+    // Привязка методов к this
+    this.clickBtn = this.clickBtn.bind(this);
+    this.setRequestField = this.setRequestField.bind(this);
   }
 
-  clickBtn(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  clickBtn(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
     console.log('click');
     console.log(this.searchRequest);
@@ -29,9 +42,9 @@ export default class Header extends React.Component {
           <input
             type="text"
             placeholder={this.searchRequest}
-            onChange={(e) => this.setRequestField(e)}
+            onChange={this.setRequestField}
           ></input>
-          <button className="header__button" onClick={(e) => this.clickBtn(e)}>
+          <button className="header__button" onClick={this.clickBtn}>
             Search
           </button>
         </form>
